3d2f6b9efb8b3ae75cc53cf9a0a75a1b172cc2e1
Mona Lisa <anastasiia.rn@gmail.com>
2023-08-15 11:27:32 -0400

    renamed seeds files from 01_users.sql to 01_users_seeds.sql created file 03_favourites_seeds.sql

---
+++ db/seeds/03_favourites_seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO favourites (user_id,listing_id) VALUES (1,8);
+INSERT INTO favourites (user_id,listing_id) VALUES (1,9);
+INSERT INTO favourites (user_id,listing_id) VALUES (1,3);
+INSERT INTO favourites (user_id,listing_id) VALUES (2,2);
+INSERT INTO favourites (user_id,listing_id) VALUES (2,3);
+INSERT INTO favourites (user_id,listing_id) VALUES (3,3);
+INSERT INTO favourites (user_id,listing_id) VALUES (4,4);
+INSERT INTO favourites (user_id,listing_id) VALUES (5,5);
+INSERT INTO favourites (user_id,listing_id) VALUES (6,6);
+INSERT INTO favourites (user_id,listing_id) VALUES (7,7);
+INSERT INTO favourites (user_id,listing_id) VALUES (8,8);
+INSERT INTO favourites (user_id,listing_id) VALUES (9,9);
+INSERT INTO favourites (user_id,listing_id) VALUES (10,10);
