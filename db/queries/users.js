8fb094423a8023626f3c9141382049e25c47f6b1
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-08-15 00:09:01 -0600

    used npm run db:reset to added schema and seeds in our database.

--- db/queries/users.js
+++ db/queries/users.js
@@ -6,5 +6,16 @@ const getUsers = () => {
       return data.rows;
     });
 };
+// Get user's products
+const getUserProducts = function(userId) {
+  return db.query(`
+    SELECT * FROM products
+    WHERE user_id = $1;
+  `, [userId])
+  .then(data => {
+    return data.rows;
+  });
+};
+
 
 module.exports = { getUsers };
