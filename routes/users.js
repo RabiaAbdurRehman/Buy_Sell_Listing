a6eae06e78f57fe32d99c31b71ce2964c8a7201c
Anastasia Reznichenko <126131921+anastasiarez@users.noreply.github.com>
2023-08-16 20:14:37 -0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ const express = require('express');
 const router  = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('module.exports = router;');
-});
+  res.render('users');
+}); 
 
 module.exports = router;
