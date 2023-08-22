75b2717c23a84770305d6871093f5a090bb43aad
Mona Lisa <anastasiia.rn@gmail.com>
2023-08-21 20:43:56 -0400

    commented out delete

--- server.js
+++ server.js
@@ -75,8 +75,8 @@ const allProductsApiRoute = require('./routes/all_products-api');
 const editProductRoute = require('./routes/edit_product');
 const editProductApiRoute = require('./routes/edit_product-api');
 
-const deleteProductRoute = require('./routes/delete_product');
-const deleteProductApiRoute = require('./routes/delete_product-api');
+// const deleteProductRoute = require('./routes/delete_product');
+// const deleteProductApiRoute = require('./routes/delete_product-api');
 
 
 // Login
@@ -110,9 +110,8 @@ app.use('/api/edit-product', editProductApiRoute);
 
 // // delete
 
-
-app.use('/delete_product', deleteProductRoute);
-app.use('/api/delete-product', deleteProductApiRoute);
+// app.use('/delete_product', deleteProductRoute);
+// app.use('/api/delete-product', deleteProductApiRoute);
 
 
 
